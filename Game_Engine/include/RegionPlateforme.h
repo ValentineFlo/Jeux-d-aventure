e99572cafc31cb9fd3d635686d3f57f51e33778c
ValentineFlo <vflorens@gaming.tech>
2025-04-06 11:59:16 +0200

    pb

--- Game_Engine/include/RegionPlateforme.h
+++ Game_Engine/include/RegionPlateforme.h
@@ -1,13 +1,14 @@
 #pragma once
 #include "Region.h"
 
-class PlateformeRegion : public IRegion, public NonDestructibleObject
+class PlateformeRegion : public IRegion, public NonDestructibleObject, public IComposite
 {
 public:
 
 	PlateformeRegion(float x, float y, float width, float height, IComposite* scene) 
         : IRegion(x, y, width, height, scene)
         , NonDestructibleObject(scene)
+        , IComposite(scene)
     {}
 
 
