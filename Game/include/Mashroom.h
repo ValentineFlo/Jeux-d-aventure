62554044ffb31ec85900e17f7e5bb8f6c3c6d38f
ValentineFlo <vflorens@gaming.tech>
2025-04-16 23:11:56 +0200

    interacted

--- Game/include/Mashroom.h
+++ Game/include/Mashroom.h
@@ -1,21 +1,21 @@
-#pragma once
-#include "IGameObject.h"
-#include "Animation.h"
-
-class Mashroom : public DestructibleObject, public IComposite
-{
-
-public:
-    Mashroom(IComposite* scene, IShapeSFML* background);
-    ~Mashroom() override;
-
-    void ProcessInput(const sf::Event& event) override;
-    void Update(const float& deltatime) override;
-    void Render() override;
-
-private:
-    IShapeSFML* m_background;
-    sf::RectangleShape m_shape;
-
-    int m_counter;
-};
+//#pragma once
+//#include "IGameObject.h"
+//#include "Animation.h"
+//
+//class Mashroom : public DestructibleObject, public IComposite
+//{
+//
+//public:
+//    Mashroom(IComposite* scene, IShapeSFML* background);
+//    ~Mashroom() override;
+//
+//    void ProcessInput(const sf::Event& event) override;
+//    void Update(const float& deltatime) override;
+//    void Render() override;
+//
+//private:
+//    IShapeSFML* m_background;
+//    sf::RectangleShape m_shape;
+//
+//    int m_counter;
+//};
