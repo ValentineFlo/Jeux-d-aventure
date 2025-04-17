753fbfe97459ee2209d2652ed76867a4c2ede014
Yoann <yoannjoseph16062000@gmail.com>
2025-04-17 17:46:35 +0200

    t

--- Scene/src/Level_Editor_Scene.cpp
+++ Scene/src/Level_Editor_Scene.cpp
@@ -1,16 +1,16 @@
-#include "Level_Editor_Scene.h"
-
-Level_Editor_Scene::Level_Editor_Scene(sf::RenderWindow* window, const float& framerate, TextureCache* texture)
-	:ISceneBase(window, framerate, texture)
-{ 
-}
-
-
-void Level_Editor_Scene::Update(const float& deltatime)
-{ }
-
-void Level_Editor_Scene::ProcessInput(const sf::Event& event) 
-{ }
-
-void Level_Editor_Scene::Render() 
-{ }
+//#include "Level_Editor_Scene.h"
+//
+//Level_Editor_Scene::Level_Editor_Scene(sf::RenderWindow* window, const float& framerate, TextureCache* texture)
+//	:ISceneBase(window, framerate, texture)
+//{ 
+//}
+//
+//
+//void Level_Editor_Scene::Update(const float& deltatime)
+//{ }
+//
+//void Level_Editor_Scene::ProcessInput(const sf::Event& event) 
+//{ }
+//
+//void Level_Editor_Scene::Render() 
+//{ }
